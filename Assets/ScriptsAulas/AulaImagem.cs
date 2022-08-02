9b7d466dcb4dcac644eddab32eb521dfc3dc20ed
Fabiano De Paula <fabiano.rpaula@gmail.com>
2022-08-02 20:45:41 -0300

    Imagem Trocando

--- Assets/ScriptsAulas/AulaImagem.cs
+++ Assets/ScriptsAulas/AulaImagem.cs
@@ -7,24 +7,47 @@ public class AulaImagem : MonoBehaviour
     public Sprite imagem1;
     public Sprite imagem2;
     public Sprite imagem3;
-
+    public int contador = 0;
     public SpriteRenderer MostradorDeImagem;
 
-   
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKey(KeyCode.D))
         {
-            MostradorDeImagem.sprite = imagem1;
-            
+
+
+            contador = contador + 1;
+            if (contador < 25)
+            {
+                MostradorDeImagem.sprite = imagem1;
+
+            }
+            if (contador < 50)
+            {
+                if (contador > 25)
+                {
+                    MostradorDeImagem.sprite = imagem2;
+                }
+            }
+            if (contador < 75)
+            {
+                if (contador > 50)
+                {
+                    MostradorDeImagem.sprite = imagem3;
+                }
+            }
+            if (contador > 75)
+            {
+                contador = 0;
+            }
+            transform.position = new Vector3(transform.position.x + 0.01f, transform.position.y, transform.position.z);
         }
-        if (Input.GetKeyDown(KeyCode.B))
+
+        if (Input.GetKey(KeyCode.A))
         {
-            MostradorDeImagem.sprite = imagem2;
-        }
-        if (Input.GetKeyDown(KeyCode.C))
-        {
-            MostradorDeImagem.sprite = imagem3;
+            transform.localScale = new Vector3(-1, 1, 1);
+            transform.position = new Vector3(transform.position.x - 0.01f, transform.position.y, transform.position.z);
         }
     }
 }
